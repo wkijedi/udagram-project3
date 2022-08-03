30c8b454bb2496b8b02b9f8939722648761efff3
wkijedi <wesleigh#1>
2022-08-03 23:34:53 +0300

    Initial Push

---
+++ udagram-api-user/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=Wesleigh#1
+export POSTGRES_HOST=cdr.cnsclzjgaph5.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::udagram-1984131-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=wesley
+export URL=http://localhost:8100
